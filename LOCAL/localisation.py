45e88b6b9988e158799d0fad2f99645ca9c06c51
Aayush64047Developer <106085120+AyushSharma64047@users.noreply.github.com>
2023-04-11 13:02:02 +0530

    Update localisation.py

--- LOCAL/localisation.py
+++ LOCAL/localisation.py
@@ -5,11 +5,11 @@ START_TEXT = "Send me any file to begin."
 
 FORCE_SUB_TEXT = "In order to use this bot, you've to join my parent channel."
 
-CHANNEL_LINK = "https://t.me/DroneBots"
+CHANNEL_LINK = "https://t.me/z_harbour"
 
-SUPPORT_LINK = "https://t.me/TeamDrone"
+SUPPORT_LINK = "https://t.me/mdisk_bots"
 
-info_text = "This bot is developed by @MaheshChauhan\n\nWritten in python library TELETHON.\n\nBot by : @DroneBots\nSupport : @TeamDrone\n\nV1.4"   
+info_text = "This bot is developed by dev @sigma_male_007\n\nWritten in python library TELETHON.\n\nBot by : @MdiskBots\nSupport : @z_harbour\n\nV1.4"   
 
 help_text = """**v1.4**
 
@@ -29,13 +29,13 @@ help_text = """**v1.4**
 
 source_text = "**Deploy your own bot**"
 
-DEV = "https://t.me/MaheshChauhan"
+DEV = "https://t.me/sigma_male_007"
 
-spam_notice = "This bot is hosted on heroku, and hence can just run one process at a time.Spamming the bot or encoding adult videos will lead you to a ban."
+spam_notice = "This bot is hosted on okteto, and hence can just run one process at a time.Spamming the bot or encoding adult videos is allowed with exceptions via bot."
 
 JPG = "LOCAL/video_convertor.jpg"
 
-JPG0 = 'https://telegra.ph/file/d98c559b56ef884ef3bad.jpg'
+JPG0 = 'https://te.legra.ph/file/21055562cc50a9c080d9a.jpg'
 
 JPG2 = "LOCAL/20211215_165751.jpg"
 
