c15e3041f3b138087913557a20cf086d0b09643f
Aayush Sharma <bhaskarsharma64047@gmail.com>
2023-04-12 13:08:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Ayush64047/MdiskAdvance /MdiskPrince 
+  git clone https://github.com/AyushSharma64047/VIDEOconvertor /VIDEOconvertor 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /MdiskPrince 
+  git clone $UPSTREAM_REPO /VIDEOconvertor 
 fi
-cd /MdiskPrince 
+cd /VIDEOconvertor 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
